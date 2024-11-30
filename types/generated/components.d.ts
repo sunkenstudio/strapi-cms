6c3770be4c632e5a76a91696969a9aec9c426223
Daniel Warrick <danielwarrick@Daniels-MacBook-Pro.local>
2024-11-30 18:06:10 -0500

    support for admin route. Consolidated component folders into just 'common', had to update some fields to lower case to get things to work. Long term we're probably going to want to convert all fields to lowercase for consistency and implement some kind of style guide.

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -13,6 +13,21 @@ export interface CommonArticle extends Schema.Component {
   };
 }
 
+export interface CommonBorder extends Schema.Component {
+  collectionName: 'components_common_borders';
+  info: {
+    displayName: 'Border';
+    description: '';
+  };
+  attributes: {
+    width: Attribute.String;
+    color: Attribute.Enumeration<
+      ['primary', 'secondary', 'accent', 'dark', 'light']
+    >;
+    radius: Attribute.String;
+  };
+}
+
 export interface CommonBrandColor extends Schema.Component {
   collectionName: 'components_common_brand_colors';
   info: {
@@ -40,7 +55,6 @@ export interface CommonButton extends Schema.Component {
     Text: Attribute.String;
     Href: Attribute.String;
     Icon: Attribute.String;
-    Border: Attribute.Component<'styles.border'>;
     Color: Attribute.Enumeration<
       ['primary', 'secondary', 'accent', 'light', 'dark']
     >;
@@ -48,6 +62,7 @@ export interface CommonButton extends Schema.Component {
       ['primary', 'secondary', 'accent', 'light', 'dark']
     >;
     Shadow: Attribute.String;
+    Border: Attribute.Component<'common.border'>;
   };
 }
 
@@ -61,6 +76,37 @@ export interface CommonFeature extends Schema.Component {
     Header: Attribute.String & Attribute.Required;
     Blurb: Attribute.String & Attribute.Required;
     Icon: Attribute.String & Attribute.Required;
+  };
+}
+
+export interface CommonFilter extends Schema.Component {
+  collectionName: 'components_common_filters';
+  info: {
+    displayName: 'Filter';
+    description: '';
+  };
+  attributes: {
+    color: Attribute.Enumeration<
+      ['primary', 'secondary', 'accent', 'light', 'dark']
+    >;
+    opacity: Attribute.Decimal &
+      Attribute.SetMinMax<
+        {
+          min: 0;
+          max: 1;
+        },
+        number
+      > &
+      Attribute.DefaultTo<0.5>;
+    grayscale: Attribute.Integer &
+      Attribute.SetMinMax<
+        {
+          min: 0;
+          max: 100;
+        },
+        number
+      > &
+      Attribute.DefaultTo<0>;
   };
 }
 
@@ -85,56 +131,10 @@ export interface CommonImage extends Schema.Component {
     description: '';
   };
   attributes: {
-    Media: Attribute.Media & Attribute.Required;
-    Alt: Attribute.String;
-    Border: Attribute.Component<'styles.border'>;
-    Filter: Attribute.Component<'styles.filter'>;
-  };
-}
-
-export interface StylesBorder extends Schema.Component {
-  collectionName: 'components_styles_borders';
-  info: {
-    displayName: 'Border';
-    icon: 'brush';
-  };
-  attributes: {
-    Width: Attribute.String;
-    Color: Attribute.Enumeration<
-      ['primary', 'secondary', 'accent', 'dark', 'light']
-    >;
-    Radius: Attribute.String;
-  };
-}
-
-export interface StylesFilter extends Schema.Component {
-  collectionName: 'components_styles_filters';
-  info: {
-    displayName: 'Filter';
-    icon: 'brush';
-  };
-  attributes: {
-    Color: Attribute.Enumeration<
-      ['primary', 'secondary', 'accent', 'light', 'dark']
-    >;
-    Opacity: Attribute.Decimal &
-      Attribute.SetMinMax<
-        {
-          min: 0;
-          max: 1;
-        },
-        number
-      > &
-      Attribute.DefaultTo<0>;
-    Grayscale: Attribute.Integer &
-      Attribute.SetMinMax<
-        {
-          min: 0;
-          max: 100;
-        },
-        number
-      > &
-      Attribute.DefaultTo<0>;
+    media: Attribute.Media & Attribute.Required;
+    alt: Attribute.String;
+    border: Attribute.Component<'common.border'>;
+    filter: Attribute.Component<'common.filter'>;
   };
 }
 
@@ -142,13 +142,13 @@ declare module '@strapi/types' {
   export module Shared {
     export interface Components {
       'common.article': CommonArticle;
+      'common.border': CommonBorder;
       'common.brand-color': CommonBrandColor;
       'common.button': CommonButton;
       'common.feature': CommonFeature;
+      'common.filter': CommonFilter;
       'common.font': CommonFont;
       'common.image': CommonImage;
-      'styles.border': StylesBorder;
-      'styles.filter': StylesFilter;
     }
   }
 }
